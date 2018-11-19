1e1b6776a8f6c4204ac6faf9183026f508cee033
coelhofb@gmail.com <coelhofb@gmail.com>
2018-11-19 13:45:50 -0600

    Twitter Data

---
+++ FC/TwitterData/twtkey.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "QViHFjeeCwN4Ac3Uou6GwhnL0"
+consumer_secret = "SiBU1eoBwWWhl2iBmHfGM2j1BGVEQAXg4MjBDmp0ammxzn0qkZ"
+access_token = "239179159-FD2HsYALWsS59xj3TsY3GXMAHmw2lm1hDhrF6pP2"
+access_token_secret = "r8myv7Is3YHV95cJMeFBAGvOBcmCqVed1vShgV4hCeUaD"
